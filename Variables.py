c9acc02732ae16c559c30cb96827c8afe4a733b3
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-07 23:33:12 +0600

    adding the types of variables exist in python

---
+++ Variables.py
@@ -0,0 +1,27 @@
+# Integer
+age = 30
+
+# Float
+height = 5.9
+
+# String
+name = "Alice"
+
+# Boolean
+is_student = True
+
+# List
+fruits = ["apple", "banana", "cherry"]
+
+# Tuple
+coordinates = (10.0, 20.0)
+
+# Dictionary
+person = {
+    "name": "Alice",
+    "age": 30,
+    "is_student": True
+}
+
+# Display variable types
+print(type(age), type(height), type(name), type(is_student), type(fruits), type(coordinates), type(person))
