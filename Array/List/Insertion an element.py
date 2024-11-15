c6cb3079789efc12529028109e69307c259d1613
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-15 22:46:08 +0600

    adding list insertion

---
+++ Array/List/Insertion an element.py
@@ -0,0 +1,11 @@
+# Explanation
+print("Syntax: list.insert(index, value)")
+print("The insert() method adds a value to the list at the specified index.")
+
+# Example code
+my_list = [1, 2, 4, 5]  # Initial list
+print("Initial list:", my_list)
+
+# Insert a new value at index 2
+my_list.insert(2, 3)  # Inserts the value 3 at index 2
+print("List after inserting 3 at index 2:", my_list)
